d45fefcb5bf498b02c54b142486c736e96962e18
gnahc2305 <achangmurillo@gmail.com>
2022-12-05 15:02:10 -0500

    add motion to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,26 @@
-import React from 'react'
+import React from "react";
+import { motion } from "framer-motion";
+import { navVariants } from "../utils/motion";
 
 function navbar() {
   return (
-    <nav className='h-14 w-[100%] flex justify-between items-center px-6'>
-
+    <motion.nav
+      variants={navVariants}
+      initial="hidden"
+      whileInView="show"
+      className="h-14 w-[100%] flex justify-between items-center px-6"
+    >
       <form>
         <label htmlFor="language">Language: </label>
-        <select name="language" id="language" className=''>
+        <select name="language" id="language" className="">
           <option value="english">English</option>
           <option value="spanish">Spanish</option>
         </select>
       </form>
 
-      <button className='rounded-xl h-[50%]'>Mode</button>
-    </nav>
-  )
+      <button className="rounded-xl h-[50%]">Mode</button>
+    </motion.nav>
+  );
 }
 
-export default navbar
+export default navbar;
