19c0d21334d2648b7d1cf1021e1754e9ee3666f4
gnahc2305 <achangmurillo@gmail.com>
2022-12-04 23:03:19 -0500

    finish profile

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,24 +1,28 @@
 import React from "react";
 import { motion } from "framer-motion";
+import { staggerContainer, textVariant } from "../utils/motion";
 
 function Profile() {
   return (
     <section>
-      <div className="flex justify-center mt-10 sm:block">
+      <div className="mt-10 md:justify-center flex flex-col md:flex-row items-center md:items-start">
         <div>
           <img src="/profile.png" alt="profile" />
         </div>
 
-        <div className='max-w-[400px] xl:max-w-[600px] px-5'>
-          <h1 className="text-[50px]">Andres Chang</h1>
-          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquid omnis necessitatibus doloremque beatae totam vel vitae consequatur non at, provident autem nostrum itaque fugit.</p>
+        <div className="max-w-[400px] xl:max-w-[600px] text-center md:text-start md:px-5">
+          <motion.h1 animate={textVariant(1.1)} className="text-[50px]">
+            {/* <h1 className="text-[50px]">Andres Chang</h1> */}
+            Andres Chang
+          </motion.h1>
+        
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquid
+            omnis necessitatibus doloremque beatae totam vel vitae consequatur
+            non at, provident autem nostrum itaque fugit.
+          </p>
         </div>
-
       </div>
-
-      {/* <div>
-        <img src="/profile.png" alt="profile" />
-      </div> */}
     </section>
   );
 }
