9a41278c66120c850d787e927a09a4b334937fa5
gnahc2305 <achangmurillo@gmail.com>
2022-12-06 13:44:00 -0500

    fix projects heading too small

--- src/components/CustomTexts.jsx
+++ src/components/CustomTexts.jsx
@@ -4,7 +4,7 @@ import { textContainer, textVariant2 } from "../utils/motion";
 export const TypingText = ({ title, textStyles }) => (
   <motion.p
     variants={textContainer}
-    className={`${textStyles} font-normal text-[14px] text-secondary-white`}
+    className={`${textStyles} font-normal text-secondary-white`}
   >
     {Array.from(title).map((letter, index) => (
       <motion.span variants={textVariant2} key={index}>
