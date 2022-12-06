4375d3478d7dbed406b90e41c41a6f7ceb5ac286
gnahc2305 <achangmurillo@gmail.com>
2022-12-06 17:48:13 -0500

    change npm png

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   const [mode, setMode] = useState('light');
 
   return (
-    <div className={mode === 'light' ? 'bg-white' : 'bg-slate-800'}>
+    <div className={`${mode === 'light' ? 'bg-white' : 'bg-slate-800'} max-w-full`}>
       <Navbar setLanguage={setLanguage} setMode={setMode} mode={mode} />
       <Profile language={language} mode={mode} />
       <StackInfo language={language} mode={mode} />
