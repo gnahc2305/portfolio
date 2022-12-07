ab302eb9b54cf011c9cd047df4c6b973f143a86c
gnahc2305 <achangmurillo@gmail.com>
2022-12-06 19:50:22 -0500

    remove darkmode css file

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './styles/index.css';
-import './styles/darkmode.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
